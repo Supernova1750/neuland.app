e0de947404788250bb5d60c0082352197660ef34
Supernova <supernova1750@protonmail.com>
2022-03-27 12:25:23 +0200

    i18n Integration for Survey card

--- rogue-thi-app/components/cards/SurveyPrompt.js
+++ rogue-thi-app/components/cards/SurveyPrompt.js
@@ -6,30 +6,34 @@ import Card from 'react-bootstrap/Card'
 import { faPoll, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import { useTranslation } from 'next-i18next'
+import TranslateDangerous from '../TranslateDangerous'
+
 import styles from '../../styles/Home.module.css'
 
 const SURVEY_URL = process.env.NEXT_PUBLIC_SURVEY_URL
 
 export default function SurveyPrompt ({ onHide }) {
+  const { t } = useTranslation()
+
   return (
     <Card className={styles.card}>
       <Card.Body>
         <Card.Title>
           <FontAwesomeIcon icon={faPoll} fixedWidth />
           {' '}
-          Umfrage
+          {  t("homecards.survey.title") }
           <Button variant="link" className={styles.cardButton} onClick={() => onHide()}>
-            <FontAwesomeIcon title="Schließen" icon={faTimes} />
+            <FontAwesomeIcon title={ t("general.close") } icon={faTimes} />
           </Button>
         </Card.Title>
         <Card.Text>
           <p>
-            Die Studierendenvertretung möchte mit den Verkehrsbetrieben ein <strong>Semesterticket</strong> aushandeln.
-            Wir möchten euch alle darum bitten, eure Meinung dazu zu äußern:
+            <TranslateDangerous i18nKey="homecards.survey.description"/>
           </p>
           <p>
             <a href={SURVEY_URL} target="_blank" rel="noreferrer">
-              <Button variant="outline-secondary">An Umfrage teilnehmen</Button>
+              <Button variant="outline-secondary">{ t("homecards.survey.link-text") }</Button>
             </a>
           </p>
         </Card.Text>
